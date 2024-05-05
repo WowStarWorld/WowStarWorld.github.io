0ac9f8d4c3a9d8481e46b0eca10fd9de5a8d5238
WowStarWorld <98941734+WowStarWorld@users.noreply.github.com>
2024-05-05 09:07:40 +0000

    Update gh-pages to output generated at 58c02ee

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+
+import MenuBar from "./components/MenuBar.vue";
+import MyAvatar from "./components/MyAvatar.vue";
+import MyTools from "./components/MyTools.vue";
+</script>
+
+<template>
+	<div style="width: 100%;">
+		<MenuBar/>
+		<div style="display: flex; justify-content: center; margin-top: 4rem;">
+			<MyAvatar :size="250"/>
+		</div>
+		<div style="display: flex; justify-content: center;">
+			<h1 style="background-clip: text; color: transparent; background-image: linear-gradient(#eeaa50, #eeee50);">StarWorld</h1>
+		</div>
+		<div style="display: flex; justify-content: center;">
+			<MyTools/>
+		</div>
+	</div>
+</template>
+
+<style scoped lang="scss">
+</style>
---
+++ src/components/MyTools.vue
@@ -0,0 +1,17 @@
+<template>
+	<el-button @click="gotoToolBox">
+		<i class="fa fa-toolbox"/>
+		&nbsp;
+		Toolbox
+	</el-button>
+</template>
+
+<script setup lang="ts">
+function gotoToolBox() {
+	location.href = "https://WowStarWorld.github.io/StarWorldToolBox-Plus";
+}
+</script>
+
+<style scoped>
+
+</style>
